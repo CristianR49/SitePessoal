7138a0fdc02a58aabe48ccceca19d87700cb8b14
LucasDoK <lucasdonadel27@gmail.com>
2025-07-22 10:34:55 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-console.log("AIIIIIII")
+// const divCompetencias = document.getElementById('div-competencias')
+// const competencias = ["C#", ".NET", "Entity Framework", "Java", "Angular", "TypeScript", "HTML", "CSS", "Bootstrap"]
 
-const divCompetencias = document.getElementById('div-competencias')
-const competencias = ["C#", ".NET", "Entity Framework", "Java", "Angular", "TypeScript", "HTML", "CSS", "Bootstrap"]
+// for (let i = 0; i < competencias.length; i++) {
+//   let divCompetencia = document.createElement('div');
+//   divCompetencia.classList.add('div-competencia');
 
-for (let i = 0; i < competencias.length; i++) {
-  let divCompetencia = document.createElement('div');
-  divCompetencia.classList.add('div-competencia');
+//   let textoCompetencia = document.createElement('h4');
+//   textoCompetencia.classList.add('h-competencia');
 
-  let textoCompetencia = document.createElement('h4');
-  textoCompetencia.classList.add('h-competencia');
+//   textoCompetencia.textContent = competencias[i];
+//   divCompetencia.appendChild(textoCompetencia)
 
-  textoCompetencia.textContent = competencias[i];
-  divCompetencia.appendChild(textoCompetencia)
-
-  divCompetencias.appendChild(divCompetencia)
-}
+//   divCompetencias.appendChild(divCompetencia)
+// }
 
